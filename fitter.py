32cc29bb25c75d7e105451f0fd93781922338485
Davide Alimonti <davide12.teramo@gmail.com>
2025-06-02 23:44:44 +0200

    Fixed power law

--- fitter.py
+++ fitter.py
@@ -6,8 +6,29 @@ def guinier(q,a,b,bg):
     #Please note that b = R^2 / 3
     return a* exp( - b* q**2 ) + bg
 
-def porod(q,a,n,c):
-    return a*pow(n) + c
+def power_law(q,a,n):
+    #Porod law if n=-4
+    return a*np.power(q,n)
+
+def fit_guinier_from_file(filein,maxq):
+    """
+    Fit Guinier law on a I(q) spectrum which is formatted as two tab separated columns 
+    which contain :
+    q I(q)
+    Allows comments after "#"
+    """
+    qs,iqs=[],[]
+    with open(filein,'r') as fin:
+        for l in fin.readlines():
+            if l.startswith('#'):
+                continue
+            data = [ float(x) for x in l.strip().split() ]
+            if data[0] < maxq :
+                qs.append(data[0])
+                iqs.append(data[1])
+    params,covar=curve_fit(guinier,qs, iqs, p0=[1000,10,10])
+    gyr_rad = sqrt(params[1]*3)
+    return gyr_rad,params,covar
 
 def fit_guinier_from_file(filein,maxq):
     """
