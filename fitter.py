0aa6a7d5ca70e03c35baa83e46256c144fdf3f15
Davide Alimonti <davide12.teramo@gmail.com>
2025-06-02 23:45:22 +0200

    Fixed power law

--- fitter.py
+++ fitter.py
@@ -29,23 +29,3 @@ def fit_guinier_from_file(filein,maxq):
     params,covar=curve_fit(guinier,qs, iqs, p0=[1000,10,10])
     gyr_rad = sqrt(params[1]*3)
     return gyr_rad,params,covar
-
-def fit_guinier_from_file(filein,maxq):
-    """
-    Fit Guinier law on a I(q) spectrum which is formatted as two tab separated columns 
-    which contain :
-    q I(q)
-    Allows comments after "#"
-    """
-    qs,iqs=[],[]
-    with open(filein,'r') as fin:
-        for l in fin.readlines():
-            if l.startswith('#'):
-                continue
-            data = [ float(x) for x in l.strip().split() ]
-            if data[0] < maxq :
-                qs.append(data[0])
-                iqs.append(data[1])
-    params,covar=curve_fit(guinier,qs, iqs, p0=[1000,10,10])
-    gyr_rad = sqrt(params[1]*3)
-    return gyr_rad,params,covar
